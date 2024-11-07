5fd317230602dea7437f2896035be1a280e3e66a
JaminYC <a20212422@pucp.edu.pe>
2024-11-07 10:27:58 -0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Importa dotenv
 import 'package:pucpflow/features/app/splash_screen/splash_screen.dart';
 import 'package:pucpflow/features/user_auth/presentation/pages/home_page.dart';
 import 'package:pucpflow/features/user_auth/presentation/pages/login_page.dart';
@@ -9,6 +9,7 @@ import 'package:pucpflow/features/user_auth/presentation/pages/sign_up_page.dart
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // Carga las variables de entorno
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
